39c341c4fc1d6ed51568c28316f61e310c45e995
Ilan Joselevich <personal@ilanjoselevich.com>
2021-12-25 02:33:56 +0200

    Add CI

--- flake.nix
+++ flake.nix
@@ -1,11 +1,15 @@
 {
   description = "A flake for developing and building my personal website";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.deepthought = { url = "github:RatanShreshtha/DeepThought"; flake = false; };
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    deepthought = { url = "github:RatanShreshtha/DeepThought"; flake = false; };
+    flake-compat-ci.url = "github:hercules-ci/flake-compat-ci";
+    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
+  };
 
-  outputs = { self, nixpkgs, flake-utils, deepthought }:
+  outputs = { self, nixpkgs, flake-utils, deepthought, flake-compat, flake-compat-ci }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
@@ -24,13 +28,19 @@
           buildPhase = "zola build";
           installPhase = "cp -r public $out";
         };
+
         defaultPackage = self.packages.${system}.website;
+
         devShell = pkgs.mkShell {
           packages = with pkgs; [ zola nodePackages.gramma ];
           shellHook = ''
             mkdir -p themes
             ln -sn "${deepthought}" "themes/${themeName}"
           '';
+        };
+
+        ciNix = flake-compat-ci.lib.recurseIntoFlakeWith {
+          flake = self;
         };
       }
     );
